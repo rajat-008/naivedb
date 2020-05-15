b67cb6659363b666fd5fac2c4fbd11e4abb9cc40
rajat-008 <mailrajat008@gmail.com>
2020-05-15 22:01:34 +0530

    added insert view

--- app.py
+++ app.py
@@ -3,15 +3,26 @@ from core import NaiveDB
 app=Flask(__name__)
 db=NaiveDB("naivedb/")
 
-
+@app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
 
+@app.route("/insert",methods=['GET','POST'])
+def insert():
+    if request.method=="GET":
+        return render_template("insert.html")
+    data=request.form.to_dict()
+    db.anime.insert(data)
+    return redirect(url_for("render_record",pkey=data["name"]))
+
+@app.route("/record/<string:pkey>")
+def render_record(pkey):
+    data=db.anime.search(pkey)
+    return render_template("display_record.html",data=data)
+
 @app.route("/search",methods=["POST"])
 def search():
     name=request.form.get("name")
-    print(name)
-    data=db.anime.search(name)
-    print(data)
-    return render_template("search.html",data=data)
+    return redirect(url_for("render_record",pkey=name))
+
